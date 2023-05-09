19da25cbb0696be83d76ee81bd3422d675fa1e9d
Yoann Alves <yoannalves7@gmail.com>
2023-05-09 15:14:59 +0200

    structure correcte de l'alphabet

--- terre00.py
+++ terre00.py
@@ -1,2 +1,8 @@
 ############### L’alphabet ###############
 
+alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+for letter in alphabet:
+    print(letter, end="")
+else:
+    print("")
