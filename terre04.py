ae7767ba8bfd5823b1f1613ce32a1ff477217e34
Yoann Alves <yoannalves7@gmail.com>
2023-05-10 16:14:14 +0200

    cette fois c'est bon même si c'est pas parfait

--- terre04.py
+++ terre04.py
@@ -1,25 +1,30 @@
 ############### Pair ou impair ###############
 
-#après 7 heures de code j'ai réussi OUIIIIII, je suis trop content ^^
+#après 8 heures de code j'ai réussi OUIIIIII, je suis trop content ^^
 
 import sys
 
 search_number = sys.argv
 
+def error():
+    print("Tu ne me la mettras pas à l'envers")
+
 if len(search_number) < 2:
-    print("Tu ne me la mettras pas à l'envers")
+    error()
 elif len(search_number) > 2:
-    print("Tu ne me la mettras pas à l'envers")
+    error()
 else:
     count = 0
     for chart in range(0,10):
         is_int = search_number[1].count(f"{count}")
         count = count + 1
         if is_int > 0:
+            if search_number[1].count("."):
+                raise ValueError("Tu ne me la mettras pas à l'envers")
             if int(search_number[1]) % 2 == 0:
                 print("pair")
             else:
                 print("impair")
-            break
+            break      
     else:
-        print("Tu ne me la mettras pas à l'envers")
+        error()
