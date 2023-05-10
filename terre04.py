434213fe88f712c4f42264de8e90eda3e35a0a8a
Yoann Alves <yoannalves7@gmail.com>
2023-05-10 10:43:51 +0200

    fonction pais impair ok

--- terre04.py
+++ terre04.py
@@ -1,1 +1,13 @@
 ############### Pair ou impair ###############
+
+import sys
+
+number = sys.argv[1]
+
+def even_odd(number):
+    if number % 2 == 0:
+        print("pair")
+    elif number % 2 == 0:
+        print("impair")
+
+
