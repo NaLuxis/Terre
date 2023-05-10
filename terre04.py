23e56008365ea5c0122f5705223eb4e066b42e61
Yoann Alves <yoannalves7@gmail.com>
2023-05-10 15:44:14 +0200

    terre04 finiiiiiii

--- terre04.py
+++ terre04.py
@@ -1,28 +1,25 @@
 ############### Pair ou impair ###############
 
-#récupérer les arguments
-#s'asurer qu'il y en ai le bon nombre
-#s'asurer qu'il soit du bon type
-#si j'ia un argument valide je dit s'il et pair ou impair
+#après 7 heures de code j'ai réussi OUIIIIII, je suis trop content ^^
 
 import sys
 
 search_number = sys.argv
-search_type = search_number[1]
 
 if len(search_number) < 2:
     print("Tu ne me la mettras pas à l'envers")
 elif len(search_number) > 2:
     print("Tu ne me la mettras pas à l'envers")
 else:
-    pass
-
-
-def even_odd(number):
-    if number % 2 == 0:
-        print("pair")
+    count = 0
+    for chart in range(0,10):
+        is_int = search_number[1].count(f"{count}")
+        count = count + 1
+        if is_int > 0:
+            if int(search_number[1]) % 2 == 0:
+                print("pair")
+            else:
+                print("impair")
+            break
     else:
-        print("impair")
-
-if int(search_type):
-    print(even_odd(int(search_number)))
+        print("Tu ne me la mettras pas à l'envers")
