56121dd7eef87964a799783ef4a110e54438b335
Yoann Alves <yoannalves7@gmail.com>
2023-05-11 18:27:04 +0200

    fonctionne

--- terre10.py
+++ terre10.py
@@ -1,1 +1,49 @@
 ############### Nombre premier ###############
+
+import sys
+
+def main() -> None:
+    """Point d'entrée du programme """
+    valide_arg: int = validate_arg()
+    # sqrt_result: int = sqrt(valide_arg)
+    result: int = prime_number(validate_arg())
+    print(result)
+
+
+def validate_arg() -> int:
+    """Vérifie qu'il n'y a qu'un nombre positif qui est donné"""
+    if len(sys.argv) != 2:
+        print(f"Erreur, le nombres d'arguments entré(s) ({len(sys.argv) - 1}) est incorect. Nombre attendus : 1")
+        exit()
+    elif sys.argv[1].isdecimal() != True:
+        if sys.argv[1].count("+"):
+            pass
+        elif sys.argv[1].count("-"):
+            print("Erreur. Seuls les nombres positifs sont acceptés")
+            exit()
+        else:
+            print("Erreur. Un nombre entier est attendus")
+            exit()
+    elif int(sys.argv[1]) < 2:
+        print(f"Erreur. {sys.argv[1]} n'est pas considéré comme un nombre premier")
+        exit()
+    return int(sys.argv[1])
+
+
+def sqrt(arg: int) -> int:
+    """Cacul de la racine carrée d'un nombre"""
+    return arg ** 0.5
+
+
+def prime_number(arg: int) -> str:
+    """Calcul si un nombre est premier"""
+    for num in range(2,arg + 1):
+        if arg % num == 0 and num != arg:
+            print(f"Non, {arg} n'est pas un nombre premier")
+            exit()
+        elif num == arg:
+            print(f"Oui, {arg} est un nombre premier.")
+            exit()
+
+if __name__ == "__main__":
+    main()
