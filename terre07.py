4248f04b7077c20f860e3300f068f83b1054588d
Yoann Alves <yoannalves7@gmail.com>
2023-05-11 11:39:02 +0200

    fini pour la v1

--- terre07.py
+++ terre07.py
@@ -1,1 +1,29 @@
 ############### Taille d’une chaîne ###############
+
+import sys
+
+def main() -> None:
+    """Point d'entrée du programme """
+    arg: str = strig_validate()
+    length_result: int = string_length(arg)
+    print(length_result)
+
+
+def strig_validate() -> str:
+    """Regarde si l'argument est valide"""
+    if len(sys.argv) !=2:
+        print("erreur.")
+        exit()
+    if sys.argv[1].isnumeric():
+        print("erreur.")
+        exit()
+    return sys.argv[1]
+
+
+def string_length(string_arg: str) -> int:
+    """Compte le nombre de caractères dans une chaine de caractères donnée en paramètre"""
+    return len(string_arg)
+
+
+if __name__ == "__main__":
+    main()
