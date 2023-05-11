eef2fa93a54fbb9491342c07111765fcea0cb47c
Yoann Alves <yoannalves7@gmail.com>
2023-05-11 08:31:52 +0200

    tout bon

--- terre05.py
+++ terre05.py
@@ -1,17 +1,5 @@
 ############### Divisions ###############
 
-#je récupère les 2 nombres OK
-#je défini de 1er nombre et le deuxième au bon endroit OK
-#La division par 0 n'est pas possible OK
-#diviser un nombre par un nombre plus grand n'est pas possible OK
-
-#j'ai une fonction qui prendra les deux nombres et fait le calcul
-#la fonction arrondie le résultat 
-#elle retour le reste
-
-#j'ai un print pour le rétultat
-#j'ai un print pour le reste
-
 import sys
 
 dividend = int(sys.argv[1])
