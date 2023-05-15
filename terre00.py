25094f63006cf8a8c23a17962ca895e8ba2db5a9
Yoann Alves <yoannalves7@gmail.com>
2023-05-15 10:36:35 +0200

    Version 2

--- terre00.py
+++ terre00.py
@@ -1,8 +1,23 @@
 ############### L’alphabet ###############
 
-alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+def main() -> None:
+    """Point d'entrée du programme"""
+    printed_alphabet: str = print_alphabet()
+    print(printed_alphabet)
 
-for letter in alphabet:
-    print(letter.lower(), end="")
-else:
-    print("")
+
+def print_alphabet() -> str:
+    """Affiche l'alphabet entier en minuscules"""
+    
+    alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+    for letter in alphabet:
+        print(letter.lower(), end="")
+    else:
+        print("")
+        exit()
+
+
+if __name__ == "__main__":
+    main()
+    exit()
